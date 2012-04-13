355864b7b8d551245372afe0f2c59aacbef3cf58
CookieDev <dev@casual4.de>
2012-04-13 12:52:57 +0200

    init

---
+++ wp-content/themes/green-belt/header.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The Header for our theme.
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 * We filter the output of wp_title() a bit -- see
+	 * twentyten_filter_wp_title() in functions.php.
+	 */
+	wp_title( '|', true, 'right' );
+
+	?></title>
+	
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+
+<?php
+	/* We add some JavaScript to pages with the comment form
+	 * to support sites with threaded comments (when in use).
+	 */
+	if ( is_singular() && get_option( 'thread_comments' ) )
+		wp_enqueue_script( 'comment-reply' );
+
+	/* Always have wp_head() just before the closing </head>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to add elements to <head> such
+	 * as styles, scripts, and meta tags.
+	 */
+	wp_head();
+?>
+</head>
+
+<body <?php body_class(); ?>>
+	
+	<!-- We use a wrapper so we can center all the content and give it a max width in the css -->
+	<div id="wrapper">
+	
+		<header id="oben">
+			<hgroup>
+				<h1><?php bloginfo('name'); ?></h1>
+				<p><?php bloginfo('description'); ?></p>
+			</hgroup>
+		</header>
+		
+		<nav id="menu" role="main">
+			<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+		</nav>
+
